e11a5ca025ca7e7742b0a3eddb6c6b5d6d5b1208
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-01 18:35:18 +0100

    day1 : part 2 is working

--- day1/day1.go
+++ day1/day1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 )
 
@@ -26,7 +27,6 @@ func openFile() []string {
 
 func part1(list []string) {
 	sum := int64(0)
-
 	for _, element := range list {
 		i := 0
 		firstDigit, err := strconv.ParseInt(string(element[i]), 10, 0)
@@ -43,7 +43,34 @@ func part1(list []string) {
 	fmt.Println(sum)
 }
 
+func part2(list []string) {
+	sum := 0
+	mapValue := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+	ruleRegex := ""
+	for value := range mapValue {
+		ruleRegex += string(value) + "|"
+	}
+	ruleRegex = ruleRegex[:len(ruleRegex)-1]
+	fmt.Println(ruleRegex)
+	for _, element := range list {
+		firstValue := regexp.MustCompile(ruleRegex).FindString(element)
+		firstDigit := mapValue[firstValue]
+		secondDigit := 0
+		for i := len(element); i >= 0; i-- {
+			secondValue := regexp.MustCompile(ruleRegex).FindString(element[i:])
+			if secondValue != "" {
+				secondDigit = mapValue[secondValue]
+				break
+			}
+		}
+		sum += 10*firstDigit + secondDigit
+	}
+	fmt.Println(sum)
+}
+
 func main() {
 	list := openFile()
 	part1(list)
+	part2(list)
 }
