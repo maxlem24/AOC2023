3f4d37546714c6f6df335e1a231d388ba1a759eb
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-06 08:47:10 +0100

    day6 : part2 is working

--- day6/day6.go
+++ day6/day6.go
@@ -45,6 +45,48 @@ func openFile() [][2]int {
 	return races
 }
 
+func openFile2() [][2]int {
+	ruleNumber := regexp.MustCompile("[0-9]+")
+	buffer, err := os.ReadFile("day6/day6.txt")
+	if err != nil {
+		panic(err)
+	}
+	races := [][2]int{}
+	line := ""
+	i := 0
+	for string(buffer[i]) != "\n" {
+		if string(buffer[i]) != " " {
+			line += string(buffer[i])
+		}
+		i++
+	}
+	i++
+	times := ruleNumber.FindAllString(line, -1)
+	line = ""
+	for string(buffer[i]) != "\n" {
+		if string(buffer[i]) != " " {
+			line += string(buffer[i])
+		}
+		i++
+	}
+	distances := ruleNumber.FindAllString(line, -1)
+	if len(distances) != len(times) {
+		panic("Lecture !")
+	}
+	for i := 0; i < len(distances); i++ {
+		time, err := strconv.ParseInt(times[i], 10, 0)
+		if err != nil {
+			panic(err)
+		}
+		distance, err := strconv.ParseInt(distances[i], 10, 0)
+		if err != nil {
+			panic(err)
+		}
+		races = append(races, [2]int{int(time), int(distance)})
+	}
+	return races
+}
+
 func part1(races [][2]int) {
 	product := 1
 	for _, race := range races {
@@ -61,7 +103,26 @@ func part1(races [][2]int) {
 	fmt.Println(product)
 }
 
+func part2(races [][2]int) {
+	product := 1
+	for _, race := range races {
+		time, distance := race[0], race[1]
+		tMin:= 0
+		for tMin * (time - tMin) < distance {
+			tMin++
+		}
+		tMax:= time
+		for tMax * (time - tMax) < distance {
+			tMax--
+		}
+		product *= (tMax-tMin+1)
+	}
+	fmt.Println(product)
+}
+
 func main() {
 	races := openFile()
 	part1(races)
+	races2 := openFile2()
+	part2(races2)
 }
