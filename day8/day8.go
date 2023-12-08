8a0635a83be8643374f544f60d551ae498cb3e2e
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-08 19:16:42 +0100

    day8 : part2 is working

--- day8/day8.go
+++ day8/day8.go
@@ -6,6 +6,18 @@ import (
 	"regexp"
 )
 
+func LCM(a int, b int) int {
+	i := 1
+	for j := 1; i*a != j*b; {
+		if i*a < j*b {
+			i++
+		} else if j*b < i*a {
+			j++
+		}
+	}
+	return i * a
+}
+
 func openFile() ([]int, map[string][2]string) {
 	buffer, err := os.ReadFile("day8/day8.txt")
 	nodeNames := regexp.MustCompile("\\w{3}")
@@ -42,14 +54,39 @@ func openFile() ([]int, map[string][2]string) {
 func part1(instructions []int, names map[string][2]string) {
 	i := 0
 	for node := "AAA"; node != "ZZZ"; {
-		side := instructions[i % len(instructions)]
+		side := instructions[i%len(instructions)]
 		node = names[node][side]
 		i++
 	}
 	fmt.Println(i)
 }
 
+func part2(instructions []int, names map[string][2]string) {
+	value := 1
+	for gear1 := 0; gear1 < 26; gear1++ {
+		for gear2 := 0; gear2 < 26; gear2++ {
+
+			node := string(65+gear1) + string(65+gear2) + "A"
+			if names[node] != [2]string{"", ""} {
+				i := 0
+				for string(node[2]) != "Z"{
+					side := instructions[i%len(instructions)]
+					node = names[node][side]
+					i++
+				}
+				if value == 1{
+					value = i
+				}else {
+					value = LCM(value, i)
+				}
+			}
+		}
+	}
+	fmt.Println(value)
+}
+
 func main() {
 	instructions, names := openFile()
 	part1(instructions, names)
+	part2(instructions, names)
 }
