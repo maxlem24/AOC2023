0a01394fbb7377f58f2fb81295ee082c26e02738
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-08 18:14:14 +0100

    day7 : part1 is working

--- day7/day7.go
+++ day7/day7.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	// "regexp"
 	"strconv"
 	"strings"
 )
@@ -98,7 +97,96 @@ func part1(cardsBid [][2]string) {
 	}
 	value := 0
 	for index, hand := range orderedCard {
-		bid, err := strconv.ParseInt(hand[1],10,0)
+		bid, err := strconv.ParseInt(hand[1], 10, 0)
+		if err != nil {
+			panic(err)
+		}
+		value += int(bid) * (index + 1)
+	}
+	fmt.Println(value)
+}
+
+func order2(hand1 string, hand2 string) bool { // true if card 1 > card 2
+	cards1 := map[string]int{}
+	for i := 0; i < len(hand1); i++ {
+
+		if cards1[string(hand1[i])] != 0 {
+			cards1[string(hand1[i])]++
+		} else {
+			cards1[string(hand1[i])] = 1
+		}
+	}
+	cards2 := map[string]int{}
+	for i := 0; i < len(hand2); i++ {
+		if cards2[string(hand2[i])] != 0 {
+			cards2[string(hand2[i])]++
+		} else {
+			cards2[string(hand2[i])] = 1
+		}
+	}
+	max1c1 := 0
+	max2c1 := 0
+	for card, nbcard := range cards1 {
+		if nbcard > max1c1 && card != "J" {
+			max2c1 = max1c1
+			max1c1 = nbcard
+		} else if nbcard > max2c1 && card != "J" {
+			max2c1 = nbcard
+		}
+	}
+	max1c2 := 0
+	max2c2 := 0
+	for card, nbcard := range cards2 {
+		if nbcard > max1c2 && card != "J" {
+			max2c2 = max1c2
+			max1c2 = nbcard
+		} else if nbcard > max2c2 && card != "J" {
+			max2c2 = nbcard
+		}
+	}
+	max1c1 += cards1["J"]
+	max1c2 += cards2["J"]
+	if max1c1 > max1c2 {
+		return true
+	} else if max1c1 < max1c2 {
+		return false
+	} else if max2c1 > max2c2 {
+		return true
+	} else if max2c1 < max2c2 {
+		return false
+	} else {
+		value := map[string]int{"A": 14, "K": 13, "Q": 12, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "J": 0}
+		for i := 0; i < 5; i++ {
+			if value[string(hand1[i])] < value[string(hand2[i])] {
+				return false
+			} else if value[string(hand1[i])] > value[string(hand2[i])] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func part2(cardsBid [][2]string) {
+	orderedCard := [][2]string{}
+	for _, hand := range cardsBid {
+		toAdd := true
+		for i := 0; i < len(orderedCard); i++ {
+			if order2(orderedCard[i][0], hand[0]) {
+				orderedCard = append(orderedCard[:i+1], orderedCard[i:]...)
+				orderedCard[i] = hand
+				toAdd = false
+				break
+			}
+		}
+		if toAdd {
+			orderedCard = append(orderedCard, hand)
+		}
+	}
+	value := 0
+	for index, hand := range orderedCard {
+
+		bid, err := strconv.ParseInt(hand[1], 10, 0)
 		if err != nil {
 			panic(err)
 		}
@@ -110,5 +198,5 @@ func part1(cardsBid [][2]string) {
 func main() {
 	cardBid := openFile()
 	part1(cardBid)
-	// part2(races)
+	part2(cardBid)
 }
