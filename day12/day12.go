6f96370434b20d734132bb3269d00a1f0eee832e
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-13 13:26:49 +0100

    day12 : part2 is working

--- day12/day12.go
+++ day12/day12.go
@@ -53,9 +53,23 @@ func sumArray(array []int) int {
 	return sum
 }
 
+func mergeMaps(m1 map[string]int, m2 map[string]int) map[string]int {
+	merged := make(map[string]int)
+	for key, value := range m1 {
+		merged[key] = value
+	}
+	for key, value := range m2 {
+		merged[key] = value
+	}
+	return merged
+}
+
 func recordValid(record string, values []int) bool {
 	currentvalue := 0
 	i := 0
+	if sumArray(values)+len(values)-1 > len(record) {
+		return false
+	}
 	for i < len(record) {
 		if string(record[i]) == "?" {
 			return true
@@ -77,7 +91,7 @@ func recordValid(record string, values []int) bool {
 				return false
 			}
 			currentvalue++
-			if i == len(record){
+			if i == len(record) {
 				if currentvalue != len(values) {
 					return false
 				}
@@ -87,7 +101,7 @@ func recordValid(record string, values []int) bool {
 				return false
 			}
 		}
-		i++;
+		i++
 	}
 	if currentvalue != len(values) {
 		return false
@@ -95,38 +109,78 @@ func recordValid(record string, values []int) bool {
 	return true
 }
 
-func backtracking(record string, values []int, listIndex []int) int {
+func backtracking(record string, values []int, memory *map[string]int) int {
+	ruleShard := regexp.MustCompile("#+")
+	ruleQuestion := regexp.MustCompile("\\?")
 	value := 0
-	if len(listIndex) == 0 {
+	index := ruleQuestion.FindStringIndex(record)
+	if index == nil {
 		return 1
 	}
-	recordShard := replaceAtIndex(record, '#', listIndex[0])
-	recordDot := replaceAtIndex(record, '.', listIndex[0])
+	recordShard := replaceAtIndex(record, '#', index[0])
 	if recordValid(recordShard, values) {
-		value += backtracking(recordShard, values, listIndex[1:])
+		offset := 0
+		alreadyCheck := 0
+		if index[0] !=0 && string(recordShard[index[0]-1]) == "." {
+			offset = index[0]
+			alreadyCheck = len(ruleShard.FindAllString(recordShard[:index[0]-1], -1))
+		}
+		key := recordShard[offset:]
+		for _, length := range values[alreadyCheck:] {
+			key += strconv.Itoa(length)
+		}
+		val, exist := (*memory)[key]
+		if !exist {
+			val = backtracking(recordShard[offset:], values[alreadyCheck:], memory)
+			(*memory)[key] = val
+		}
+		value += val
 	}
+	recordDot := replaceAtIndex(record, '.', index[0])
 	if recordValid(recordDot, values) {
-		value += backtracking(recordDot, values, listIndex[1:])
+		alreadyCheck := len(ruleShard.FindAllString(recordDot[:index[0]], -1))
+		key := recordDot[index[0]+1:]
+		for _, length := range values[alreadyCheck:] {
+			key += strconv.Itoa(length)
+		}
+		val, exist := (*memory)[key]
+		if !exist {
+			val = backtracking(recordDot[index[0]+1:], values[alreadyCheck:], memory)
+			(*memory)[key] = val
+		}
+		value += val
 	}
 	return value
 }
 
 func part1(records []string, values [][]int) {
-	ruleQuestion := regexp.MustCompile("\\?")
+
 	sum := 0
 	if len(records) != len(values) {
 		panic("Size Problem")
 	}
 	for i, record := range records {
-		questionIndex := []int{}
-		allIndex := ruleQuestion.FindAllStringIndex(record, -1)
-		if allIndex == nil {
-			continue
+		memory := map[string]int{}
+		sum += backtracking(record, values[i], &memory)
+	}
+	fmt.Println(sum)
+}
+
+func part2(records []string, values [][]int) {
+	sum := 0
+	if len(records) != len(values) {
+		panic("Size Problem")
+	}
+	for i, record := range records {
+		fmt.Print(i, "/", len(records), "\r")
+		newValue := values[i]
+		newRecord := record
+		for k := 0; k < 4; k++ {
+			newRecord += "?" + record
+			newValue = append(newValue, values[i]...)
 		}
-		for _, index := range allIndex {
-			questionIndex = append(questionIndex, index[0])
-		}
-		sum += backtracking(record, values[i], questionIndex)
+		memory := map[string]int{}
+		sum += backtracking(newRecord, newValue, &memory)
 	}
 	fmt.Println(sum)
 }
@@ -134,4 +188,5 @@ func part1(records []string, values [][]int) {
 func main() {
 	records, values := openFile()
 	part1(records, values)
+	part2(records, values)
 }
