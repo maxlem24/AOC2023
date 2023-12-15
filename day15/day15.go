a85679bed0e61cb08760005a8edce56a6a0bc280
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-15 18:45:17 +0100

    day15 : part2 is working

--- day15/day15.go
+++ day15/day15.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
+
+type lens struct {
+	name  string
+	focal int
+}
 
 func openFile() []string {
 	buffer, err := os.ReadFile("day15/day15.txt")
@@ -37,7 +44,60 @@ func hash(instruction string) int {
 func part1(sequence []string) {
 	sum := 0
 	for _, instruction := range sequence {
-		sum +=  hash(instruction)
+		sum += hash(instruction)
+	}
+	fmt.Println(sum)
+}
+
+func removeLens(boxs [256][]lens, label string) [256][]lens {
+	boxNumber := hash(label)
+	for i, lensInTheBox := range boxs[boxNumber] {
+		if lensInTheBox.name == label {
+			boxs[boxNumber] = append(boxs[boxNumber][:i], boxs[boxNumber][i+1:]...)
+			break
+		}
+	}
+	return boxs
+}
+
+func addLens(boxs [256][]lens, label string, focal int) [256][]lens {
+	boxNumber := hash(label)
+	toAdd := true
+	for i, lensInTheBox := range boxs[boxNumber] {
+		if lensInTheBox.name == label {
+			boxs[boxNumber][i] = lens{label, focal}
+			toAdd = false
+			break
+		}
+	}
+	if toAdd {
+		boxs[boxNumber] = append(boxs[boxNumber], lens{label, focal})
+	}
+	return boxs
+}
+
+func part2(sequence []string) {
+	ruleNumber := regexp.MustCompile("\\d")
+	ruleLabel := regexp.MustCompile("[a-zA-Z]+")
+	sum := 0
+	boxs := [256][]lens{}
+	for _, instruction := range sequence {
+		label := ruleLabel.FindString(instruction)
+		focalString := ruleNumber.FindString(instruction)
+		if focalString == "" {
+			boxs = removeLens(boxs, label)
+		} else {
+			focal, err := strconv.ParseInt(focalString, 10, 0)
+			if err != nil {
+				panic(err)
+			}
+			boxs = addLens(boxs, label, int(focal))
+		}
+	}
+	for i, box := range boxs {
+		for j, lens := range box {
+			sum += (i+1)*(j+1)*lens.focal
+		}
 	}
 	fmt.Println(sum)
 }
@@ -45,4 +105,5 @@ func part1(sequence []string) {
 func main() {
 	sequence := openFile()
 	part1(sequence)
+	part2(sequence)
 }
